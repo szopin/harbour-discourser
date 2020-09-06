2bc855aa8f58accfc4371f8a90994e1020ea9b6d
Åke Engelbrektson <eson57@users.noreply.github.com>
2020-09-06 16:44:17 +0200

    Create harbour-discourser-sv.ts
    Add Swedish translation

---
+++ translations/harbour-discourser-sv.ts
@@ -0,0 +1,267 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="46"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="86"/>
+        <source>Discourser for Sailfish OS v%1
+By szopin
+Licensed under MIT
+
+App icon by dseight
+Special thanks to contributors:
+</source>
+        <translation>Discourser för Sailfish OS v%1
+Av szopin
+Licensierad under MIT
+
+Appikon av dseight
+Speciellt tack till medverkande:
+</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="93"/>
+        <source>, </source>
+        <extracomment>contributors list separator</extracomment>
+        <translation>, </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="103"/>
+        <source>Source code</source>
+        <translation>Källkod</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="115"/>
+        <source>License</source>
+        <translation>Licens</translation>
+    </message>
+</context>
+<context>
+    <name>CategorySelect</name>
+    <message>
+        <location filename="../qml/pages/CategorySelect.qml" line="51"/>
+        <source>Nothing to show</source>
+        <translation>Inget att visa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CategorySelect.qml" line="52"/>
+        <source>Is the network enabled?</source>
+        <translation>Är nätverket aktiverat?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CategorySelect.qml" line="61"/>
+        <source>Categories</source>
+        <translation>Kategorier</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CategorySelect.qml" line="69"/>
+        <source>Latest</source>
+        <translation>Senast</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CategorySelect.qml" line="76"/>
+        <source>Top</source>
+        <translation>Topp</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="45"/>
+        <source>Latest posts</source>
+        <translation>Senaste inläggen</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="112"/>
+        <source>SailfishOS</source>
+        <extracomment>part of &apos;SailfishOS Forum Viewer&apos;</extracomment>
+        <translation>SailfishOS</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="122"/>
+        <source>Forum Viewer</source>
+        <extracomment>part of &apos;SailfishOS Forum Viewer&apos;</extracomment>
+        <translation>Forumvisare</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="97"/>
+        <source>Latest</source>
+        <translation>Senast</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="105"/>
+        <source>Top</source>
+        <translation>Topp</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="153"/>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="157"/>
+        <source>Change forum</source>
+        <translation>Byt forum</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="161"/>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="166"/>
+        <source>Reload</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="183"/>
+        <source>Nothing to show</source>
+        <translation>Inget att visa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="184"/>
+        <source>Is the network enabled?</source>
+        <translation>Är nätverket aktiverat?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="296"/>
+        <source>Load more</source>
+        <translation>Hämta mer</translation>
+    </message>
+</context>
+<context>
+    <name>OpenLink</name>
+    <message>
+        <location filename="../qml/pages/OpenLink.qml" line="36"/>
+        <source>Open URL in webview?</source>
+        <translation>Vill du öppna URL:en i webbvy?</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/OpenLink.qml" line="52"/>
+        <source>Open externally</source>
+        <translation>Öppna externt</translation>
+    </message>
+</context>
+<context>
+    <name>PostView</name>
+    <message>
+        <location filename="../qml/pages/PostView.qml" line="50"/>
+        <source>Previous</source>
+        <translation>Föregående</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PostView.qml" line="56"/>
+        <source>Next</source>
+        <translation>Nästa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PostView.qml" line="67"/>
+        <source>Revision history</source>
+        <translation>Revisionshistorik</translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="51"/>
+        <source>Searching in “%1”</source>
+        <translation>Söker i &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="77"/>
+        <source>No results</source>
+        <extracomment>part of &apos;No results in &quot;foo&quot;&apos;</extracomment>
+        <translation>Inga träffar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="79"/>
+        <source>in “%1”</source>
+        <extracomment>part of &apos;No results in &quot;foo&quot;&apos;</extracomment>
+        <translation>i &quot;%1&quot;</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="163"/>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="170"/>
+        <source>Search in all threads</source>
+        <translation>Sök i alla trådar</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="171"/>
+        <source>Search in the current thread</source>
+        <translation>Sök i aktuell tråd</translation>
+    </message>
+</context>
+<context>
+    <name>SitePage</name>
+    <message>
+        <location filename="../qml/pages/SitePage.qml" line="56"/>
+        <source>Custom URL</source>
+        <translation>Anpassad URL</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SitePage.qml" line="57"/>
+        <source>Just part between second and third slash
+(no &quot;https://&quot; and no trailing slash)</source>
+        <translation>Bara delen mellan andra och tredje snedstrecket
+(inget &quot;https://&quot; och inget avslutande snedstreck)</translation>
+    </message>
+</context>
+<context>
+    <name>ThreadView</name>
+    <message>
+        <location filename="../qml/pages/ThreadView.qml" line="111"/>
+        <source>Open in external browser</source>
+        <translation>Öppna i extern webbläsare</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ThreadView.qml" line="115"/>
+        <source>Open directly</source>
+        <translation>Öppna direkt</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ThreadView.qml" line="120"/>
+        <source>Search thread</source>
+        <translation>Sök tråd</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/pages/ThreadView.qml" line="174"/>
+        <source>%n like(s)</source>
+        <translation>
+            <numerusform>%n like</numerusform>
+            <numerusform>%n likes</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ThreadView.qml" line="190"/>
+        <source>✍️: %1</source>
+        <translation>✍️: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ThreadView.qml" line="213"/>
+        <source>Revision history</source>
+        <translation>Revisionshistorik</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-discourser</name>
+    <message>
+        <location filename="../qml/harbour-discourser.qml" line="48"/>
+        <source>d/M/yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</source>
+        <extracomment>date format including date and time but no weekday</extracomment>
+        <translation>yyyy-M-d (hh:mm)</translation>
+    </message>
+</context>
+</TS>
