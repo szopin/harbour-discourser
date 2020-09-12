f9be5d973538a8789a0579b4671626304a58764e
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2020-09-12 18:05:32 +0800

    Create harbour-discourser-zh_CN.ts

---
+++ translations/harbour-discourser-zh_CN.ts
@@ -0,0 +1,264 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="46"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="86"/>
+        <source>Discourser for Sailfish OS v%1
+By szopin
+Licensed under MIT
+App icon by dseight
+Special thanks to contributors:
+</source>
+        <translation>旗鱼系统版 Discourserv%1
+开发者为 sszpoin
+采用 MIT 许可协议
+软件图标由 dseight 绘制
+特别感谢贡献者们：</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="93"/>
+        <source>, </source>
+        <extracomment>contributors list separator</extracomment>
+        <translation>，</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="103"/>
+        <source>Source code</source>
+        <translation>源代码</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="115"/>
+        <source>License</source>
+        <translation>许可协议</translation>
+    </message>
+</context>
+<context>
+    <name>CategorySelect</name>
+    <message>
+        <location filename="../qml/pages/CategorySelect.qml" line="51"/>
+        <source>Nothing to show</source>
+        <translation>没有可供现实的内容</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CategorySelect.qml" line="52"/>
+        <source>Is the network enabled?</source>
+        <translation>是否已连接到网络？</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CategorySelect.qml" line="61"/>
+        <source>Categories</source>
+        <translation>分类</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CategorySelect.qml" line="69"/>
+        <source>Latest</source>
+        <translation>最新</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CategorySelect.qml" line="76"/>
+        <source>Top</source>
+        <translation>顶部</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="45"/>
+        <source>Latest posts</source>
+        <translation>最近帖子</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="112"/>
+        <source>SailfishOS</source>
+        <extracomment>part of &apos;SailfishOS Forum Viewer&apos;</extracomment>
+        <translation>旗鱼系统</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="122"/>
+        <source>Forum Viewer</source>
+        <extracomment>part of &apos;SailfishOS Forum Viewer&apos;</extracomment>
+        <translation>论坛查看器</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="97"/>
+        <source>Latest</source>
+        <translation>最近</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="105"/>
+        <source>Top</source>
+        <translation>顶部</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="153"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="157"/>
+        <source>Change forum</source>
+        <translation>更改论坛</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="161"/>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="166"/>
+        <source>Reload</source>
+        <translation>重新加载</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="183"/>
+        <source>Nothing to show</source>
+        <translation>没有可供显示的内容</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="184"/>
+        <source>Is the network enabled?</source>
+        <translation>是否已连接到网络？</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/FirstPage.qml" line="296"/>
+        <source>Load more</source>
+        <translation>加载更多</translation>
+    </message>
+</context>
+<context>
+    <name>OpenLink</name>
+    <message>
+        <location filename="../qml/pages/OpenLink.qml" line="36"/>
+        <source>Open URL in webview?</source>
+        <translation>是否通过 webview 打开超链接？</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/OpenLink.qml" line="52"/>
+        <source>Open externally</source>
+        <translation>使用外部软件打开</translation>
+    </message>
+</context>
+<context>
+    <name>PostView</name>
+    <message>
+        <location filename="../qml/pages/PostView.qml" line="50"/>
+        <source>Previous</source>
+        <translation>前一页</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PostView.qml" line="56"/>
+        <source>Next</source>
+        <translation>后一页</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/PostView.qml" line="67"/>
+        <source>Revision history</source>
+        <translation>修改历史</translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="51"/>
+        <source>Searching in “%1”</source>
+        <translation>在 %1 中搜索</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="77"/>
+        <source>No results</source>
+        <extracomment>part of &apos;No results in &quot;foo&quot;&apos;</extracomment>
+        <translation>无结果</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="79"/>
+        <source>in “%1”</source>
+        <extracomment>part of &apos;No results in &quot;foo&quot;&apos;</extracomment>
+        <translation>在 %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="163"/>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="170"/>
+        <source>Search in all threads</source>
+        <translation>在全部帖子中搜索</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SearchPage.qml" line="171"/>
+        <source>Search in the current thread</source>
+        <translation>在当前帖子中搜索</translation>
+    </message>
+</context>
+<context>
+    <name>SitePage</name>
+    <message>
+        <location filename="../qml/pages/SitePage.qml" line="56"/>
+        <source>Custom URL</source>
+        <translation>自定义超链接</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SitePage.qml" line="57"/>
+        <source>Just part between second and third slash
+(no &quot;https://&quot; and no trailing slash)</source>
+        <translation>分离第二及第三部分 slash</translation>
+    </message>
+</context>
+<context>
+    <name>ThreadView</name>
+    <message>
+        <location filename="../qml/pages/ThreadView.qml" line="111"/>
+        <source>Open in external browser</source>
+        <translation>用外部浏览器打开</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ThreadView.qml" line="115"/>
+        <source>Open directly</source>
+        <translation>直接打开</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ThreadView.qml" line="120"/>
+        <source>Search thread</source>
+        <translation>搜索帖子</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../qml/pages/ThreadView.qml" line="174"/>
+        <source>%n like(s)</source>
+        <translation>
+            <numerusform>%n 个赞</numerusform>
+            <numerusform>%n 个赞</numerusform>
+            <numerusform>%n 个赞</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ThreadView.qml" line="190"/>
+        <source>✍️: %1</source>
+        <translation>✍️: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ThreadView.qml" line="213"/>
+        <source>Revision history</source>
+        <translation>修改历史</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-discourser</name>
+    <message>
+        <location filename="../qml/harbour-discourser.qml" line="48"/>
+        <source>d/M/yyyy &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</source>
+        <extracomment>date format including date and time but no weekday</extracomment>
+        <translation>yyyy/M/d &apos;(&apos;hh&apos;:&apos;mm&apos;)&apos;</translation>
+    </message>
+</context>
+</TS>
